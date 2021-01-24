02c07441319b603e79412e6bb0efde6201e0dc8f
Burak GUL <burak.gul@ozguryazilim.com.tr>
2021-01-24 19:30:08 +0300

    initial commit

--- src/components/header/Icon.js
+++ src/components/header/Icon.js
@@ -1,5 +1,5 @@
 const Icon = (props) => {
-    return <li><a href={props.href}><i className={'icon-'+props.name+'2'}></i></a></li>
+    return <li><a href={props.href} target="_blank"><i className={'icon-'+props.name+'2'}></i></a></li>
 }
 
 export default Icon;
