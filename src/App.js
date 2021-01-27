478184abaac784e8a87fcd70d8feda1c86eff9ca
Burak GUL <burak.gul@ozguryazilim.com.tr>
2021-01-28 00:48:30 +0300

    image load improvement

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import Resume from "./components/resume/Resume";
-import Header from "./components/header/Header";
-import Feature from "./components/feature/Feature";
-import Skill from "./components/skill/Skill";
-import Started from "./components/started/Started";
-import Work from "./components/work/Work";
-import Blog from "./components/blog/Blog";
-import Footer from "./components/footer/Footer";
-import { useState } from "react";
-import React from "react";
-import { profileInit } from "./profileInit";
-
-const components = {
-  about: About,
-  contact: Contact,
-  resume: Resume,
-  feature: Feature,
-  skill: Skill,
-  started: Started,
-  blog: Blog,
-  work: Work,
-};
+import { useEffect, useState } from "react";
+import Page from "./components/Page";
 
 const App = () => {
-  const [store] = useState(profileInit);
+  const[loaderState,setLoaderState] = useState(true);
+
+  useEffect(() => {
+    setLoaderState(false);
+  },[])
 
   return (
-    <div className="page">
-      <Header {...store}/>
-      {store.components.map((componentType, index) => {
-        return React.createElement(components[componentType], {
-          key: index,
-          ...store,
-        });
-      })}
-      <Footer {...store}/>
+    <div>
+    {loaderState ? 	<div className="loader"></div> : <Page />}
     </div>
   );
 };
