b1b43ba5fc7c29b3e57439608dc1942357aadff4
burak <burakkggul@gmail.com>
2024-04-29 19:22:30 +0300

    footer has been updated.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 		<div className="container">
 			<div className="row">
 				<div className="col-md-12 text-center">
-					<p>&copy; 2023 Burak GÜL.<br/>Licensed under the <a href="https://github.com/burakkggul/profile/blob/master/LICENSE" rel="noreferrer" target="_blank">GNU General Public License, Version 3.</a><br/>Developed by <a href="https://github.com/burakkggul" rel="noreferrer" target="_blank">Burak GÜL</a>. Home page design made by <a href="https://technext.it/" rel="noreferrer" target="_blank">Technext</a>.</p>
+					<p>&copy; 2024 Burak GÜL.<br/>Licensed under the <a href="https://github.com/burakkggul/profile/blob/master/LICENSE" rel="noreferrer" target="_blank">GNU General Public License, Version 3.</a><br/>Developed by <a href="https://github.com/burakkggul" rel="noreferrer" target="_blank">Burak GÜL</a>. Home page design made by <a href="https://technext.it/" rel="noreferrer" target="_blank">Technext</a>.</p>
 				</div>
 			</div>
 		</div>
